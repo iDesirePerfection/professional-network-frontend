6e4041c11d58e0bd02cfcfb8e8968e2610e69ac8
yassinehaoues <yassine.houes@esprit>
2019-11-26 16:35:33 +0100

    authentification

---
+++ src/app/core/user-core/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user-services/user.service';
+import { User } from 'src/app/services/user-services/models/user';
+import { Usr } from './usr';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+  }
+  us:Usr=new Usr();
+  login(form) {
+    console.log(form);
+    this.userService.login('fahd','fahd123').subscribe(u=>console.log(u));
+
+  }
+
+}
---
+++ src/app/core/user-core/login/usr.ts
@@ -0,0 +1,4 @@
+export class Usr{
+    username:string;
+    password:string;
+}
