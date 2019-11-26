be4e5d9d7acfabe65f61bc4e2cb667eeaeeb0df5
iDesirePerfection <fahd-chargui@hotmail.com>
2019-11-26 16:44:47 +0100

    addition to profile

--- src/app/pages/candidate-pages/profile-page/profile-page.module.ts
+++ src/app/pages/candidate-pages/profile-page/profile-page.module.ts
@@ -2,7 +2,7 @@ import { ProfilePageComponent } from './profile-page.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExperiencesPageModule } from '../experiences-page/experiences-page.module';
-import { MatTabsModule} from '@angular/material';
+import { MatTabsModule, MatButtonModule} from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 
@@ -15,7 +15,8 @@ import { FormsModule } from '@angular/forms';
         ExperiencesPageModule,
         MatTabsModule,
         FlexLayoutModule,
-        FormsModule
+        FormsModule,
+        MatButtonModule
     ],
     exports: [ProfilePageComponent]
   })
